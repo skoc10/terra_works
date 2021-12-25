e0eaa2caed7d9b21cfbafd3a7f70204fbd0b8239
skoc <skoc10ds@gmail.com>
2021-12-25 22:28:07 +0300

    project complete

--- Apache Servers with Terraform/main.tf
+++ Apache Servers with Terraform/main.tf
@@ -25,6 +25,7 @@ resource "aws_instance" "instance" {
   instance_type = "t2.micro"
   key_name = "key"
   security_groups = ["tf-provisioner-sg"]
+  user_data  = file("user_data.sh")
   for_each = toset(var.ec2name)
   tags = {
     Name = "Terraform ${each.value} Instance"
@@ -36,3 +37,44 @@ resource "aws_instance" "instance" {
   provisioner "local-exec" {
       command = "echo http://${self.private_ip} >> private_ip.txt"
   }
+}
+
+  output "inst-public-ip" {
+  value = [for name in var.ec2name : aws_instance.instance[name].public_ip ]
+  }
+
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "tf-provisioner-sg"
+  tags = {
+    Name = "tf-provisioner-sg"
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = 22
+      protocol = "tcp"
+      to_port = 22
+      cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+      from_port = 443
+      protocol = "tcp"
+      to_port = 443
+      cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+      from_port = 0
+      protocol = -1
+      to_port = 0
+      cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
