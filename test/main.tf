16c017224d0468a38deaf8a5bfd032f26996d10d
skoc <skoc10ds@gmail.com>
2022-01-26 23:45:22 +0300

    ecr

--- test/main.tf
+++ test/main.tf
@@ -13,20 +13,28 @@ terraform {
   }
 }
 
-data "template_file" "init" {
-  template = file("${path.module}/userdata.sh")
+resource "aws_s3_bucket" "skoc1101010koc" {
+  bucket = "skoc1101010koc"
+  acl    = "private"
 
+  tags = {
+    Name        = "skoc1101010koc"
+    Environment = "Dev"
+  }
+}
+
+data "template_file" "userdata" {
+  template = file("${path.module}/userdata.sh")
   vars = {
-    rds_endpoint = "${aws_instance.test.public_ip}"
+    rds_endpoint = "${aws_s3_bucket.skoc1101010koc.bucket_domain_name}"
   }
 }
 resource "aws_instance" "test"  {
   ami           = var.aws_ami
-
   instance_type = var.instance_type
-  security_groups = [aws_security_group.lt_sg.id]
+  security_groups = ["lt_sg"]
 
-  user_data       = data.template_file.init.rendered
+  user_data       = data.template_file.userdata.rendered
  
   key_name        = var.key_name
 }
