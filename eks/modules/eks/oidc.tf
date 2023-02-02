27a51337618a9829c807d0fba6cdbdb4e37b7486
selmankoc <selman.koc@volosoft.com>
2023-02-02 08:23:31 +0300

    Create oidc.tf

---
+++ eks/modules/eks/oidc.tf
@@ -0,0 +1,38 @@
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+
+data "aws_iam_policy_document" "eks_cluster_cni_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_cni" {
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_cni_assume_role_policy.json
+  name               = "eks-cluster-cni"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_cluster_cni.name
+}
